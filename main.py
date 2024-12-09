b5400d164e01eb24e3a35677f1010b1d21494f40
Markus Rößler <info@der-zerfleischer.de>
2024-12-09 17:35:34 +0100

    Update

--- main.py
+++ main.py
@@ -67,7 +67,7 @@ class ImageAnalyzerApp(QMainWindow):
 
     def initUI(self):
         self.setWindowTitle("Prompt from picture with llama3.2-vision | from www.der-zerfleischer.de") # ANCHOR Titel
-        self.setFixedSize(600, 620)
+        self.setFixedSize(600, 640)
 
         central_widget = QWidget()
         self.setCentralWidget(central_widget)
--- main2.py
+++ main2.py
@@ -67,7 +67,7 @@ class ImageAnalyzerApp(QMainWindow):
 
     def initUI(self):
         self.setWindowTitle("Prompt from picture with AI-Vision Models | from www.der-zerfleischer.de") # ANCHOR Titel
-        self.setFixedSize(600, 685)
+        self.setFixedSize(600, 705)
 
         central_widget = QWidget()
         self.setCentralWidget(central_widget)
